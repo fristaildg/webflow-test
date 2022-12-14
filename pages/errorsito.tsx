4c6a06eac95cc3ccf33594662510d5928e726d21
fristaildg <fristaildg@gmail.com>
2022-12-15 00:21:39 +0100

    update site

--- pages/errorsito.tsx
+++ pages/errorsito.tsx
@@ -1,30 +1,32 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import parseHtml from 'html-react-parser';
+import Link from 'next/link';
 
-type HomeProps = {
+type ErrorPageProps = {
   bodyContent: string;
   headContent: string;
 };
 
-const Home: NextPage<HomeProps> = ({ bodyContent, headContent }) => {
+const ErrorPage: NextPage<ErrorPageProps> = ({ bodyContent, headContent }) => {
   return (
     <>
       <Head>
         {parseHtml(headContent)}
       </Head>
       <div dangerouslySetInnerHTML={{ __html: bodyContent }}></div>
+      <Link href="/errorsito">Al errorsito</Link>
     </>
   )
 }
 
-export default Home
+export default ErrorPage
 
 export const getStaticProps = async () => {
   const cheerio = await import('cheerio');
   const axios = (await import('axios')).default;
 
-  const response = await axios('https://madtryoshkas.webflow.io/');
+  const response = await axios('https://adritellez.webflow.io/errorsito');
   const html = response.data;
 
   const $ = cheerio.load(html);
